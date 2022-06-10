7ada877b9f68d2f76d1218b570e02eb06fee55b0
antweny <antweny@live.com>
2022-06-10 10:39:47 +0300

    Update 2016_02_07_000000_create_likeable_tables.php
    On MYSQL 5.6  when migrating you get an error: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes.
    
    to solve it update the migration file by changing the columns likeable_id and likeable_type from $table->string type to  $table->morphs('likeable') and the

--- migrations/2016_02_07_000000_create_likeable_tables.php
+++ migrations/2016_02_07_000000_create_likeable_tables.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateLikeableTables extends Migration
 {
@@ -9,17 +10,15 @@ class CreateLikeableTables extends Migration
     {
         Schema::create('likeable_likes', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('likeable_id', 36);
-            $table->string('likeable_type', 255);
+            $table->morphs('likeable');
             $table->string('user_id', 36)->index();
             $table->timestamps();
             $table->unique(['likeable_id', 'likeable_type', 'user_id'], 'likeable_likes_unique');
         });
-        
+
         Schema::create('likeable_like_counters', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('likeable_id', 36);
-            $table->string('likeable_type', 255);
+            $table->morphs('likeable');
             $table->unsignedBigInteger('count')->default(0);
             $table->unique(['likeable_id', 'likeable_type'], 'likeable_counts');
         });
